ec639bd12aa821f31124202e4551abe4c62f5ec2
Mert Doğu <mertdoguonder@protonmail.com>
2023-04-20 11:54:54 +0300

    feat: spacing with json

--- src/loader.js
+++ src/loader.js
@@ -5,9 +5,17 @@
  */
 module.exports = (animation, text) => {
   let frames = 0;
+  let space = 1;
+
+  if (animation.space){
+    space = animation.space;      
+  }
+
+  let _space = " ".repeat(space)
+
 
   const interval = setInterval(() => {
-    process.stdout.write(`\r${animation.frames[frames++]} ${text}`);
+    process.stdout.write(`\r${animation.frames[frames++]}${_space}${text}`);
 
     frames %= animation.frames.length;
   }, 1000 / animation.fps);
