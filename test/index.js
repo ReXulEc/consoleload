cfc72a459dc7a9a4489160d5875670bf290ee383
Mert Doğu <mertdoguonder@protonmail.com>
2023-04-20 11:56:25 +0300

    test: new examples

--- test/index.js
+++ test/index.js
@@ -1,8 +1,51 @@
 const cload = require("../src/index.js");
 const { setTimeout: sleep } = require("node:timers/promises");
 
+/* New line
+
 (async () => {
   let log = cload("Fetching user...", "basicspin");
   await sleep(2000);
+  let log2 = cload("Fetching data...", "basicspin");
+  await sleep(2000);
+  log2.stop();
+})();
+*/
+
+/* Same line
+
+(async () => {
+  let log = cload("Losssading...", "plusminus");
+  await sleep(2000);
+  log.stop();
+  let log2 = cload("Loading more data...", "plusminus");
+  await sleep(2000);
+  log2.stop();
+})();
+*/
+
+/* Custom animation
+
+(async () => {
+  const MyAnimation = {
+    fps: 5,
+    frames: [
+      "-",
+      "."
+    ],
+  }
+  let log = cload("Loading with custom animation...", MyAnimation);
+  await sleep(2000);
   log.stop();
 })();
+*/
+
+/* Preset animation with custom fps
+
+(async () => {
+  let log = cload("Loading animation with 40fps...", "moon", 40);
+  await sleep(2000);
+  log.stop();
+  let log2 = cload("Loading animation with default fps...", "moon");
+})();
+*/
